94b20d3cdbc4ca36d91b446fb11ae5e37adb1377
warlyware <danward@gmail.com>
2015-09-11 14:39:18 -0400

    Add basic admin page

--- js/controllers/admin.js
+++ js/controllers/admin.js
@@ -1,4 +1,7 @@
 angular.module('myApp')
-.controller('AdminCtrl', function($scope) {
+.controller('AdminCtrl', function($scope, $http) {
   console.log('AdminCtrl loaded');
+  $http.get('/admin/classes').then(function(data) {
+    $scope.classes = data.data;
+  });
 });
